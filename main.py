fbefc2fdf36219184ee385d9d0b950e5428bb117
Owen M. Peoples <peoplowe000@warren.k12.in.us>
2024-08-26 10:04:12 -0400

    fixed it

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-frisbees = 3
-COST_OF_FRISBEES = 15
-total_cost = 15.0 * 3.0
+frisbees = 3.0
+COST_OF_FRISBEES = 15.0
+total_cost = COST_OF_FRISBEES * frisbees
 print("The total cost is $", total_cost, sep="")
